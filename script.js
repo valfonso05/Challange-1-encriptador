cf56c922aa2fd17462e1ae55c285a3833b02acff
valfonso05 <valfonso05@outlook.com>
2023-02-11 01:25:13 +0100

    Editar html y css para hacer sitio responsive

--- script.js
+++ script.js
@@ -1,45 +1,74 @@
 //Funcion para encriptar
 function encriptar() {
-  // Convierte el la entrada en mayuscula
-  var texto = document.getElementById("entradaTexto").value.toLowerCase();
-  
-  // igm es para afectar a mayusculas, minusculas, toda la linea y varias lineas.
+  // Convierte la entrada en mayuscula
+  var texto = document.getElementById("textIn").value.toLowerCase();
+
+  //valida el texto introducido
+  var input = document.getElementById("textIn").value;
+  if(!/^[a-zñ]+$/m.test(input)) {
+    alert("No se permiten caracteres especiales. ");
+    return false;
+  }
+
+  if(input){
+    // igm es para afectar a mayusculas, minusculas, toda la linea y varias lineas.
     var txtcifrado = texto.replace(/e/igm,"enter");
     var txtcifrado = txtcifrado.replace(/i/igm,"imes");
     var txtcifrado = txtcifrado.replace(/a/igm,"ai");
     var txtcifrado = txtcifrado.replace(/o/igm,"ober");  
     var txtcifrado = txtcifrado.replace(/u/igm,"ufat");
-    document.getElementById("imagenPagina").style.display = "none";
-    document.getElementById("texto").style.display = "none";
-    document.getElementById("copiar").style.display = "show";
-    document.getElementById("texto2").innerHTML = txtcifrado;
-    document.getElementById("entradaTexto").innerHTML = " ";
-    document.getElementById("copiar").style.display = "inherit";
+    document.getElementById("imgLeftside").style.display = "none";
+    document.getElementById("text").style.display = "none";
+    document.getElementById("textTwo").style.display = "none";
+    document.getElementById("btnCopy").style.display = "show";
+    document.getElementById("textEncrypted").display = "show";
+    document.getElementById("textEncrypted").innerHTML = txtcifrado;
+    document.getElementById("textIn").innerHTML = " ";
+    document.getElementById("btnCopy").style.display = "inherit";
+  }
 }
+
 
 //Funcion para desencriptar
 function desencriptar() {
-  var texto = document.getElementById("entradaTexto").value.toLowerCase();
-  var txtcifrado = texto.replace(/enter/igm,"e");
-  var txtcifrado = txtcifrado.replace(/imes/igm,"i");
-  var txtcifrado = txtcifrado.replace(/ai/igm,"a");
-  var txtcifrado = txtcifrado.replace(/ober/igm,"o");
-  var txtcifrado = txtcifrado.replace(/ufat/igm,"u");
-  document.getElementById("imagenPagina").style.display = "none";
-  document.getElementById("texto").style.display = "none";
-  document.getElementById("copiar").style.display = "show";
-  document.getElementById("texto2").innerHTML = txtcifrado;
-  document.getElementById("entradaTexto").innerHTML = " ";
-  document.getElementById("copiar").style.display = "inherit";
+  // Convierte la entrada en mayuscula
+  var texto = document.getElementById("textIn").value.toLowerCase();
+
+  //valida el texto introducido
+  var entrada = document.getElementById("textIn").value;
+  if(!/^[a-zñ-ú]+$/m.test(entrada)) {
+    alert("No se permiten caracteres especiales. ");
+    return false;
+  }
+
+  if(entrada){
+    // igm es para afectar a mayusculas, minusculas, toda la linea y varias lineas.
+    var texto = document.getElementById("textIn").value.toLowerCase();
+    var txtcifrado = texto.replace(/enter/igm,"e");
+    var txtcifrado = txtcifrado.replace(/imes/igm,"i");
+    var txtcifrado = txtcifrado.replace(/ai/igm,"a");
+    var txtcifrado = txtcifrado.replace(/ober/igm,"o");
+    var txtcifrado = txtcifrado.replace(/ufat/igm,"u");
+    document.getElementById("imgLeftside").style.display = "none";
+    document.getElementById("text").style.display = "none";
+    document.getElementById("textTwo").style.display = "none";
+    document.getElementById("btnCopy").style.display = "show";
+    document.getElementById("textEncrypted").display = "show";
+    document.getElementById("textEncrypted").innerHTML = txtcifrado;
+    document.getElementById("textIn").innerHTML = " ";
+    document.getElementById("btnCopy").style.display = "inherit";
+  }
 }
 
+
 function copia() {
-  var contenido = document.querySelector("#texto2");
+  var contenido = document.querySelector("#textEncrypted");
   contenido.select();
-  document.execCommand('copy');
+  document.execCommand('Copy');
   alert("El texto ha sido copiado.");
 }
 
+
 function limpiar() {
-  document.getElementById("entradaTexto").value="";
+  document.getElementById("textIn").value="";
 }
